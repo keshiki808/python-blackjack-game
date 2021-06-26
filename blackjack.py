0338d381b2a18a2433084314420961a8ad926bbc
keshiki808 <keshiki808@gmail.com>
2021-06-26 17:22:31 -0230

    Added specifications

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,28 @@
+# To do :
+# -Create a list to store the suit, rank, and point value for each card
+
+# -Use a list of lists to store the cards in the deck. You can use two nested loops to create the deck of cards
+
+# -Use a list of lists to store the dealer's hand and the player's hand
+
+# -When the program starts, it should read the player's money amount from a CSV/txt file named money.txt'
+
+# -The program should write the player's money amount to a file any time the data is changed'
+
+# -Store the functions for writing and reading the moneu amount in a separate module named db.py
+
+# -Handle the exception that occurs if the player can't find the data file
+
+# -Handle the exceptions that occur if the user enters a string where an integer or float value is expected
+
+# -The program should validate the bet amount.
+# >>> The minimum bet should be 5
+# >>> The maximum bet should be 1,000
+# >>> The bet can't be bigger than the player's current amount of money.
+
+# -If the money amount drops below the minimum bet(5), the program should give the player the option to buy more chips.
+
+
 def main():
     pass
 
