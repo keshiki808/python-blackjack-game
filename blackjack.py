8ef0bb91d41c9870a58bc5be2bd05df6ff6372a3
keshiki808 <keshiki808@gmail.com>
2021-06-26 20:28:34 -0230

    Added specifications and deck creator function

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,9 @@
+import copy
+
 # To do :
 # -Create a list to store the suit, rank, and point value for each card
 
-# -Use a list of lists to store the cards in the deck. You can use two nested loops to create the deck of cards
+# <<<<<<<<<<<<DONE>>>>>>>>-Use a list of lists to store the cards in the deck. You can use two nested loops to create the deck of cards
 
 # -Use a list of lists to store the dealer's hand and the player's hand
 
@@ -23,8 +25,30 @@
 # -If the money amount drops below the minimum bet(5), the program should give the player the option to buy more chips.
 
 
+def deck_creator():
+    # Function to generate a deck from by combining appending the suit and rank of cards to the point value.
+    # Uses enumeration to add point values to ranks then uses a nested loop to add each suits to the ranks + points
+    # before combining and flattening the deck to be used throughout the program.
+
+    suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
+    points = [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [10], [10], [10]]
+    ranks = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
+    for count, point in enumerate(points):
+        point.append(ranks[count])
+    deck = []
+    i = 0
+    while i < 4:
+        card_container = copy.deepcopy(points)
+        for point in card_container:
+            point.append(suits[i])
+        deck.append(card_container)
+        i += 1
+    return [item for sublist in deck for item in sublist]
+
+
 def main():
-    pass
+    deck = deck_creator()
+    print(deck)
 
 
 if __name__ == "__main__":
