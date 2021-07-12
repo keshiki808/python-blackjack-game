3974ab2f5a42a7af9fed5865abfc158d1d063c7c
keshiki808 <keshiki808@gmail.com>
2021-07-12 13:57:40 -0230

    Cleaned code, clarified functions with comments, flushed out deck module

--- blackjack.py
+++ blackjack.py
@@ -1,44 +1,10 @@
-import copy
 import db
-import random
-
 import deck
-
-# To do :
-# <<<<<<<<<<<<DONE>>>>>>>>-Create a list to store the suit, rank, and point value for each card
-
-# <<<<<<<<<<<<DONE>>>>>>>>-Use a list of lists to store the cards in the deck. You can use two nested loops to create the deck of cards
-
-# <<<<<<<<<<<<DONE>>>>>>>>-Use a list of lists to store the dealer's hand and the player's hand
-
-# <<<<<<<<<<<<DONE>>>>>>>>-When the program starts, it should read the player's money amount from a CSV/txt file named money.txt'
-
-# <<<<<<<<<<<<DONE>>>>>>>>-The program should write the player's money amount to a file any time the data is changed'
-
-# <<<<<<<<<<<<DONE>>>>>>>>-Store the functions for writing and reading the moneu amount in a separate module named db.py
-
-# <<<<<<<<<<<<DONE>>>>>>>>-Handle the exception that occurs if the player can't find the data file
-
-# <<<<<<<<<<<<DONE>>>>>>>>-Handle the exceptions that occur if the user enters a string where an integer or float value is expected
-
-# <<<<<<<<<<<<DONE>>>>>>>>-The program should validate the bet amount.
-# <<<<<<<<<<<<DONE>>>>>>>>>>> The minimum bet should be 5
-# <<<<<<<<<<<<DONE>>>>>>>>>>> The maximum bet should be 1,000
-# <<<<<<<<<<<<DONE>>>>>>>>>>> The bet can't be bigger than the player's current amount of money.
-
-# <<<<<<<<<<<<DONE>>>>>>>>>>>-If the money amount drops below the minimum bet(5), the program should give the player the option to buy more chips.
 
 # Displays the title
 def title():
     print("Welcome to blackjack")
     print()
-
-
-# Picks a random card from the deck
-def card_picker(deck, hand):
-    random_card = random.choice(deck)
-    deck.remove(random_card)
-    hand.append(random_card)
 
 
 # Accepts a hand and tabulates the value of it
@@ -93,13 +59,6 @@ def bust_checker(hand_value):
     return True if hand_value > 21 else False
 
 
-# Displays the cards in the player or dealer's hand
-def card_display(hand, hand_owner):
-    print(f"{hand_owner} CARDS:")
-    for card in hand:
-        print(f"{card[1]} of {card[2]}")
-
-
 # Accepts the user wager
 def user_bet_input(player_money):
     while True:
@@ -119,25 +78,6 @@ def user_bet_input(player_money):
             print("The entry must be a valid integer or float between 5 and 1000")
         except Exception as e:
             print(e)
-
-
-def deck_creator():
-    # Function to generate a deck from by combining appending the suit and rank of cards to the point value.
-    # Uses enumeration to add point values to ranks then uses a nested loop to add each suits to the ranks + points
-    # before combining and flattening the deck to be used throughout the program.
-
-    suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
-    points = [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [10], [10], [10]]
-    ranks = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
-    for count, point in enumerate(points):
-        point.append(ranks[count])
-    deck = []
-    for suit in suits:
-        card_container = copy.deepcopy(points)
-        for point in card_container:
-            point.append(suit)
-        deck.append(card_container)
-    return [item for sublist in deck for item in sublist]
 
 
 # Initiates player turn cycle
@@ -217,6 +157,7 @@ def buy_chips(player_chips):
                     print(
                         f"Thank you for your purchase. You now have {player_chips + money_response} chips\n"
                     )
+                    db.money_writer(player_chips + money_response)
                     return player_chips + money_response
             except ValueError:
                 print("You must enter a valid integer\n")
@@ -224,19 +165,7 @@ def buy_chips(player_chips):
             print(
                 """
                 You've decided against purchasing anymore chips.
-                You reach into your pocket and remove your wallet, 
-                your eyes widen as you open it to realize
-                your life savings that you had withdrawn to 
-                try to win big have dwindled to nothing.
-                You realize even if you had wanted to purchase 
-                more chips you couldn't. You've lost everything.
-                You've spent your last few dollars on a bus ride home.
-                A sense of dread consumes you when you
-                think that perhaps the last time you see you wife and 
-                daughter will be when you arrive home and explain what happened. 
-                You lean back on the cold bus seat and close your eyes, hoping this is 
-                merely a bad dream but you know that the reality is that your life 
-                has been consumed by the fires of gambling hell.\n """
+                You walk away from the casino in shame.\n """
             )
             quit()
         else:
@@ -244,64 +173,93 @@ def buy_chips(player_chips):
             continue
 
 
+def play_again():
+    response = input("Play again? (y/n): ")
+    return "y" if response == "y" else quit()
+
+
 def main():
     title()
-    card_deck = deck_creator()
-    player_money = db.money_reader()
-    player_money = 2
-    if player_money < 5:
-        player_money = buy_chips(player_money)
-    db.money_writer(player_money)
-    bet, player_money = user_bet_input(player_money)
-    print("Player money", player_money)
-    print(card_deck)
-    player_hand = []
-    dealer_hand = []
-    card_picker(card_deck, dealer_hand)
-    dealer_hand_value = hand_value_tabulator(dealer_hand)
-    print(f"DEALER'S SHOW CARD:\n{dealer_hand[0][1]} of {dealer_hand[0][2]}\n")
-    deck.card_image_builder(dealer_hand)  # -----------------------------
-    card_picker(card_deck, player_hand)
-    card_picker(card_deck, player_hand)
-    deck.card_image_builder(player_hand)  # -----------------------------
-    card_display(player_hand, "YOUR")
-    player_hand_value = hand_value_tabulator(player_hand)
-    player_hand_value = ace_checker(player_hand, player_hand_value)
-    print(player_hand_value)
-    hit = hit_or_stand_declaration()
-    player_bust = False
-    dealer_bust = False
-    while hit == True:
-        card_picker(card_deck, player_hand)
-        card_display(player_hand, "YOUR")
+    while True:
+        card_deck = deck.deck_creator()
+        player_money = db.money_reader()
+
+        # Option given to buy chips then bet
+        if player_money < 5:
+            player_money = buy_chips(player_money)
+        bet, player_money = user_bet_input(player_money)
+        print("Player chips:", player_money)
+        print()
+
+        # Initial setup of hands and bust status
+        player_hand = []
+        dealer_hand = []
+        player_bust = False
+        dealer_bust = False
+        player_win = False
+
+        # Card dealt to player
+        deck.card_picker(card_deck, player_hand)
+        deck.card_display(player_hand, "YOUR")
+        deck.card_image_builder(player_hand)
+
+        # Dealer reveals showcard
+        deck.card_picker(card_deck, dealer_hand)
+        dealer_hand_value = hand_value_tabulator(dealer_hand)
+        deck.card_display(dealer_hand, "DEALER'S SHOW")
+        deck.card_image_builder(dealer_hand)  # -----------------------------
+        print("-" * 60)
+
+        # Player receives second card, given option to choose ace value and hit or stand
+        deck.card_picker(card_deck, player_hand)
+        deck.card_display(player_hand, "YOUR")
+        deck.card_image_builder(player_hand)  # -----------------------------
         player_hand_value = hand_value_tabulator(player_hand)
         player_hand_value = ace_checker(player_hand, player_hand_value)
-        player_bust = bust_checker(player_hand_value)
+        hit = hit_or_stand_declaration()
         print()
-        if player_bust:
-            break
-        else:
-            hit = hit_or_stand_declaration()
+
+        while hit == True:
+            # Player receives cards until they stand or bust
+            deck.card_picker(card_deck, player_hand)
+            deck.card_display(player_hand, "YOUR")
+            deck.card_image_builder(player_hand)
+            player_hand_value = hand_value_tabulator(player_hand)
+            player_hand_value = ace_checker(player_hand, player_hand_value)
+            player_bust = bust_checker(player_hand_value)
             print()
-    while dealer_hand_value < 17:
-        if player_bust:
-            break
-        card_picker(card_deck, dealer_hand)
-        card_display(dealer_hand, "DEALER'S")
-        dealer_hand_value = hand_value_tabulator(dealer_hand)
-        dealer_hand_value = dealer_ace_checker(dealer_hand, dealer_hand_value)
-        dealer_bust = bust_checker(dealer_hand_value)
-        print()
-        if dealer_bust:
-            break
-    player_win = False
-    draw = draw_check(player_hand_value, dealer_hand_value)
-    if not draw:
-        player_win = player_victory_check(
-            player_hand_value, dealer_hand_value, player_bust, dealer_bust
-        )
-    payout(player_win, draw, bet, player_money)
-    results_display(player_win, draw)
+            if player_bust:
+                break
+            else:
+                hit = hit_or_stand_declaration()
+                print()
+
+        while dealer_hand_value < 17:
+            if player_bust:
+                break
+            # Dealer receives cards until they reach 17 then stop or until they bust.
+            deck.card_picker(card_deck, dealer_hand)
+            deck.card_display(dealer_hand, "DEALER'S")
+            deck.card_image_builder(dealer_hand)
+            dealer_hand_value = hand_value_tabulator(dealer_hand)
+            dealer_hand_value = dealer_ace_checker(dealer_hand, dealer_hand_value)
+            dealer_bust = bust_checker(dealer_hand_value)
+            print()
+            if dealer_bust:
+                break
+
+        # Determines outcome, win, loss or draw for player
+        draw = draw_check(player_hand_value, dealer_hand_value)
+        if not draw:
+            player_win = player_victory_check(
+                player_hand_value, dealer_hand_value, player_bust, dealer_bust
+            )
+
+        # Cleanup by paying out the player and displaying the results
+        payout(player_win, draw, bet, player_money)
+        results_display(player_win, draw)
+
+        play_again()
 
 
 if __name__ == "__main__":
--- deck.py
+++ deck.py
@@ -1,8 +1,13 @@
+import copy
+import random
+
+# Converts suit as a string to corresponding unicode image
 def card_stylizer(card_suit):
     card_styles = {"Spades": "♠", "Hearts": "♥", "Diamonds": "♦", "Clubs": "♣"}
     return card_styles[card_suit]
 
 
+# Builds an ascii image of the hand of cards
 def card_image_builder(cards):
     card_images = ["", "", "", "", ""]
     for card in cards:
@@ -18,3 +23,36 @@ def card_image_builder(cards):
         card_images[4] += f"-" * 9 + " "
     for part in card_images:
         print(part)
+
+
+# Picks a random card from the deck
+def card_picker(deck, hand):
+    random_card = random.choice(deck)
+    deck.remove(random_card)
+    hand.append(random_card)
+
+
+# Displays the cards in a player's hand
+def card_display(hand, hand_owner):
+    if len(hand) == 1:
+        print(f"{hand_owner} CARD:")
+    else:
+        print(f"{hand_owner} CARDS:")
+    for card in hand:
+        print(f"{card[1]} of {card[2]}")
+
+
+# Function to generate a deck from by combining appending the suit and rank of cards to the point value.
+def deck_creator():
+    suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
+    points = [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [10], [10], [10]]
+    ranks = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
+    for count, point in enumerate(points):
+        point.append(ranks[count])
+    deck = []
+    for suit in suits:
+        card_container = copy.deepcopy(points)
+        for point in card_container:
+            point.append(suit)
+        deck.append(card_container)
+    return [item for sublist in deck for item in sublist]
